b5200eb9bb499f60cbf791a9cef73c671f5af0a2
Matthew Rankin <matthew@cumulusware.com>
2020-01-02 16:22:52 -0600

    Ready for 2020.

--- lxi.go
+++ lxi.go
@@ -1,5 +1,5 @@
-// Project site: https://github.com/gotmc/visa
-// Copyright (c) 2017 The visa developers. All rights reserved.
+// Copyright (c) 2017-2020 The lxi developers. All rights reserved.
+// Project site: https://github.com/gotmc/lxi
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
 
